ea9db29a546d1e8ed0f7ec4eb86b39eada52504e
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-14 18:34:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,8 @@ def monopoly
 	    :names => {
 	      :reading_railroad => {},
 	      :pennsylvania_railroad => {},
-	      :b_and_o_railroad => {}
+	      :b_and_o_railroad => {},
+	      :shortlin_railroad => {}
 	    },
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25,
