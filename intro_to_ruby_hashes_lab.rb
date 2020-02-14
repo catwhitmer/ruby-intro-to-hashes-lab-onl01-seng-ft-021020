fda8098ef166ec1d9247ddedec9ad5ca30e95d76
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-14 18:54:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,7 +32,7 @@ def monopoly_with_third_tier
 	      :reading_railroad => {},
 	      :pennsylvania_railroad => {},
 	      :b_and_o_railroad => {},
-	      :shortline_railroad => {}
+	      :short_line_railroad => {}
 	    },
 	    :rent_in_dollars => 
 	    {
