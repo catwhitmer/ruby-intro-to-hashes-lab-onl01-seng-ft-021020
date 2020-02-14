1475109c412af04da0bb41afed3a11767a105ef2
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-14 18:49:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,16 +7,19 @@ def actor
 end
 
 def monopoly
-  :railroads => {}
+  monopoly = {
+    :railroads => {}
+    
+  }
 end
 
 def monopoly_with_second_tier
+	monopoly = {
 	:railroads => {
 	  :pieces => 4,
 	  :names => {},
 	  :rent_in_dollars => {}
 	}
-
 end
 
 def monopoly_with_third_tier
