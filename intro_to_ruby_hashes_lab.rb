0665675d15b36da6dd22fecb2b6fab5a72f333c6
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-14 18:57:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,7 +51,10 @@ def monopoly_with_fourth_tier
 	    :pieces => 4,
 	    :names => 
 	    {
-	      :reading_railroad => {},
+	      :reading_railroad => 
+	      {
+	        "mortgage" => "$100"
+	      },
 	      :pennsylvania_railroad => {},
 	      :b_and_o_railroad => {},
 	      :shortline => {}
