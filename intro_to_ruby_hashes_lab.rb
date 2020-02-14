4f56fc0b1e29fc64099c2079168ced3980db4f04
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-14 18:33:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,9 +10,14 @@ def monopoly
 	monopoly = {
 	  :railroads => {
 	    :pieces => 4
-	    :names => {}
+	    :names => {
+	      :reading_railroad => {}
+	    }
 	    :rent_in_dollars => {
-	      :one_piece_owned => 25
+	      :one_piece_owned => 25,
+	      :two_pieces_owned => 50,
+	      :three_pieces_owned => 100,
+	      :four_pieces_owned => 200
 	    }
 	  }
 	}
